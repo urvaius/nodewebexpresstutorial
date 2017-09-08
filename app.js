954745f7644e5cdef02e7b21f9369867fe70b713
Dave Arneson <urvaius@gmail.com>
2017-09-08 16:28:14 -0700

    added files

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+        res.render('index');
+
+    });
+
+app.get('/books', function (req, res) {
+        res.send('Hello books');
+
+    });
+app.listen(5000, function (err) {
+        console.log('running server on port ' + port);
+    });
