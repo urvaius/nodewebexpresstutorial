f863e1ce234ac443238e2d936c511c4d8a7b6157
Dave Arneson <urvaius@gmail.com>
2017-09-15 12:56:23 -0700

    on ejs section

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
 
+
+
+app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
-        res.render('index');
+        res.render('index', {title: 'Hello from render',list: ['a','b']});
 
     });
 
