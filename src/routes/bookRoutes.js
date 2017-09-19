40c81318499be1d09f908bf12035d027d95dfcf4
Dave Arneson <urvaius@gmail.com>
2017-09-19 09:10:37 -0700

    still working trying the .id but now working just move on to mongo most likely

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -39,14 +39,36 @@ var router = function (nav) {
         });
 
     bookRouter.route('/:id')
-        .get(function (req, res) {
+        .all(function (req, res, next) {
             var id = req.params.id;
+            var ps = new sql.PreparedStatement();
+            ps.input('id', sql.Int);
+            ps.prepare('select * from books where id = @id',
+                function (err) {
+                    ps.execute({
+                            id: req.params.id
+                        },
+                        function (err, recordset) {
+                            if (recordset.length == 0) {
+                                res.status(404).send('Not Found');
+                            } else {
+                                req.book = recordset[0];
+                                next();
+                            }
+
+                        });
+                });
+        })
+        .get(function (req, res) {
             res.render('bookView', {
                 title: 'Books',
                 nav: nav,
-                book: books[id]
+                book: req.book
             });
+
+
         });
+
     return bookRouter;
 }
 module.exports = router;
