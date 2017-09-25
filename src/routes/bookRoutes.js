f9f3fe292202dd8e7d04cb13beb57b71b6f03897
Dave Arneson <urvaius@gmail.com>
2017-09-25 14:02:05 -0700

    finished controller part again. use this as new rebase for here unless get new commit with api done

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,53 +2,15 @@ var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
+
 var router = function (nav) {
-
-    bookRouter.use(function(req,res,next){
-        if (!req.user){
-            res.redirect('/');
-        }
-        next();
-    });
+    var bookController = require('../controllers/bookController')(null, nav);
+    bookRouter.use(bookController.middleware);
     bookRouter.route('/')
-        .get(function (req, res) {
-           // var url = 'mongodb://localhost:27017/libraryApp';
-           var url = 'mongodb://urvaius:Buffy11$@arne-5-mongo1-shard-00-00-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-01-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-02-ujozx.mongodb.net:27017/libraryApp?ssl=true&replicaSet=arne-5-mongo1-shard-0&authSource=admin';
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-
-                collection.find({}).toArray(
-                    function (err, results) {
-                        res.render('bookListView', {
-                            title: 'Books',
-                            nav: nav,
-                            books: results
-                        });
-                    });
-
-            });
-        });
+        .get(bookController.getIndex);
 
     bookRouter.route('/:id')
-        .get(function (req, res) {
-            var id = new objectId(req.params.id);
-           // var url = 'mongodb://localhost:27017/libraryApp';
-           var url = 'mongodb://urvaius:Buffy11$@arne-5-mongo1-shard-00-00-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-01-ujozx.mongodb.net:27017,arne-5-mongo1-shard-00-02-ujozx.mongodb.net:27017/libraryApp?ssl=true&replicaSet=arne-5-mongo1-shard-0&authSource=admin';
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-
-                collection.findOne({
-                        _id: id
-                    },
-                    function (err, results) {
-                        res.render('bookView', {
-                            title: 'Books',
-                            nav: nav,
-                            book: results
-                        });
-                    });
-            });
-        });
+        .get(bookController.getById);
 
     return bookRouter;
 };
